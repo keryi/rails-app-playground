2048a34c41705fd6ab4fef2dd31db3eef59dd175
keryi <hti_32@yahoo.com>
2015-06-04 22:58:37 +0800

    Get the basic user model spec passed

--- giveback/spec/models/user_spec.rb
+++ giveback/spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  
+  let(:user) { build :user }
+
+  it { should validate_presence_of(:email) }
+  it { should validate_uniqueness_of(:email) }
+  it { should validate_presence_of(:password) }
 end
