e90fb4d7f348d96a101cce45f78d3f6f1350c4e0
keryi <hti_32@yahoo.com>
2015-06-07 23:56:42 +0800

    Simplify spec with shoulda-matchers helpers

--- giveback/spec/models/user_spec.rb
+++ giveback/spec/models/user_spec.rb
@@ -6,33 +6,11 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of(:email) }
   it { should validate_uniqueness_of(:email) }
   it { should validate_presence_of(:password) }
-
-  context 'blank email' do
-    it 'should not be valid' do
-      user.email = ''
-      expect(user).to be_invalid
-    end
-  end
+  it { should validate_length_of(:password).is_at_least 8 }
 
   context 'invalid email' do
     it 'should not be valid' do
       user.email = 'abc.com'
-      expect(user).to be_invalid
-    end
-  end
-
-  context 'blank password' do
-    it 'should not be valid' do
-      user.password = ''
-      user.password_confirmation = user.password
-      expect(user).to be_invalid
-    end
-  end
-
-  context 'invalid password length' do
-    it 'should not be valid' do
-      user.password = Faker::Lorem.characters 7
-      user.password_confirmation = user.password
       expect(user).to be_invalid
     end
   end
